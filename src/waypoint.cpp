96a37bd6363fa74c6a72eacdf3310fef4a16e630
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2021-05-12 23:49:25 +0200

    dae mesh

--- src/waypoint.cpp
+++ src/waypoint.cpp
@@ -18,8 +18,8 @@ geometry_msgs::Pose pose;
 
 void poseCallback(const nav_msgs::OdometryConstPtr & msg)
 {
-    pose = msg->pose.pose;
-    odom_ok = true;
+  pose = msg->pose.pose;
+  odom_ok = true;
 }
 
 // global vars for WP / setpoint
@@ -29,84 +29,104 @@ geometry_msgs::PoseStamped setpoint;
 // get a particular coordinate of a given WP
 double coord(int idx, std::string var)
 {
-    return wp[idx][var].as<double>();
+  return wp[idx][var].as<double>();
 }
 
 void writeWP(int idx)
 {
-    setpoint.pose.position.x = coord(idx, "x");
-    setpoint.pose.position.y = coord(idx, "y");
-    setpoint.pose.position.z = coord(idx, "z");
-    setpoint.pose.orientation.x = 0;
-    setpoint.pose.orientation.y = 0;
-    setpoint.pose.orientation.z = sin(coord(idx, "theta")/2);
-    setpoint.pose.orientation.w = cos(coord(idx, "theta")/2);
+  setpoint.pose.position.x = coord(idx, "x");
+  setpoint.pose.position.y = coord(idx, "y");
+  setpoint.pose.position.z = coord(idx, "z");
+  setpoint.pose.orientation.x = 0;
+  setpoint.pose.orientation.y = 0;
+  setpoint.pose.orientation.z = sin(coord(idx, "theta")/2);
+  setpoint.pose.orientation.w = cos(coord(idx, "theta")/2);
 
-    setpoint.header.stamp = ros::Time::now();
+  setpoint.header.stamp = ros::Time::now();
 }
 
 int main (int argc, char** argv)
 {
-    ros::init(argc, argv, "waypoint");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "waypoint");
+  ros::NodeHandle nh;
 
-    // subscriber
-    odom_ok = false;
-    ros::Subscriber state_sub = nh.subscribe<nav_msgs::Odometry> ("/auv/state", 1, poseCallback);
+  // subscriber
+  odom_ok = false;
+  ros::Subscriber state_sub = nh.subscribe<nav_msgs::Odometry> ("/auv/state", 1, poseCallback);
 
-    // publisher
-    ros::Publisher setpoint_pub = nh.advertise<geometry_msgs::PoseStamped>("/auv/body_position_setpoint", 1);
-    setpoint.header.frame_id = "world";
+  // publisher
+  ros::Publisher setpoint_pub = nh.advertise<geometry_msgs::PoseStamped>("/auv/body_position_setpoint", 1);
+  setpoint.header.frame_id = "world";
 
-    // load waypoints
-    std::string wp_path = ros::package::getPath("ecn_rasom") + "/config/waypoints.yaml";
-    YAML::Node node = YAML::LoadFile(wp_path);
-    // get thresholds
-    const double thr = node["threshold"].as<double>();
-    std::cout << "WP Threshold: " << thr << std::endl;
-    const double thr_angle = node["threshold_angle"].as<double>();
-    std::cout << "WP Angle Threshold: " << thr_angle << std::endl;
+  // load waypoints
+  std::string wp_path = ros::package::getPath("ecn_rasom") + "/config/waypoints.yaml";
+  YAML::Node node = YAML::LoadFile(wp_path);
+  // get thresholds
+  const double thr = node["threshold"].as<double>();
+  std::cout << "WP Threshold: " << thr << std::endl;
+  const double thr_angle = node["threshold_angle"].as<double>();
+  std::cout << "WP Angle Threshold: " << thr_angle << std::endl;
 
-    // get waypoints
-    wp = node["wp"];
-    std::cout << "Found " << wp.size() << " waypoints" << std::endl;
-    int idx;
-    for(idx = 0; idx < wp.size(); ++idx)
+  // get waypoints
+  wp = node["wp"];
+  std::cout << "Found " << wp.size() << " waypoints" << std::endl;
+  int idx;
+  for(idx = 0; idx < wp.size(); ++idx)
+  {
+    std::cout << " wp #" << idx << ": ";
+    std::cout << "x = " << coord(idx, "x");
+    std::cout << ", y = " << coord(idx, "y");
+    std::cout << ", z = " << coord(idx, "z");
+    std::cout << ", theta = " << coord(idx, "theta");
+    std::cout << std::endl;
+  }
+
+  // write first WP
+  writeWP(0);
+
+  ros::Rate rate(10);
+
+  // waypoint index
+  idx = 0;
+
+  while (ros::ok())
+  {
+
+    if(odom_ok)
     {
-        std::cout << " wp #" << idx << ": ";
-        std::cout << "x = " << coord(idx, "x");
-        std::cout << ", y = " << coord(idx, "y");
-        std::cout << ", z = " << coord(idx, "z");
-        std::cout << ", theta = " << coord(idx, "theta");
-        std::cout << std::endl;
+      // check Cartesian distance + angular distance to current wp
+      // update setpoint if needed
+      setpoint.pose.position.x;
+
+      // setpoint (current waypoint)
+      // pose (current state)
+      pose.position.x;
+
+      double theta_d = coord(idx, "theta");
+
+      pose.orientation.w; // cos(theta/2)
+      pose.orientation.z; // sin(theta/2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // publish setpoint
+      setpoint_pub.publish(setpoint);
     }
-
-    // write first WP
-    writeWP(0);
-
-    ros::Rate rate(10);
-
-    // waypoint index
-    idx = 0;
-
-    while (ros::ok())
-    {
-
-        if(odom_ok)
-        {
-            // check Cartesian distance to current wp
-            // update setpoint if needed
-
-                        
-
-
-
-
-            // publish setpoint
-            setpoint_pub.publish(setpoint);
-        }
-        ros::spinOnce();
-        rate.sleep();
-    }
+    ros::spinOnce();
+    rate.sleep();
+  }
 
 }
